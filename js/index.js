498d193bfa3e8ebac415cd80c88ba21d159340f5
rick <ricktsaiii@gmail.com>
2023-12-16 20:08:22 +0800

    ’新增‘

---
+++ js/index.js
@@ -0,0 +1,25 @@
+
+  // // Import the functions you need from the SDKs you need
+  // import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+  // import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+  // // TODO: Add SDKs for Firebase products that you want to use
+  // // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // // Your web app's Firebase configuration
+  // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyDTyw7sJTYUvoRvY_L15Uva320OpgZygAA",
+  //   authDomain: "eat23-bc9eb.firebaseapp.com",
+  //   databaseURL: "https://eat23-bc9eb-default-rtdb.firebaseio.com",
+  //   projectId: "eat23-bc9eb",
+  //   storageBucket: "eat23-bc9eb.appspot.com",
+  //   messagingSenderId: "958094949964",
+  //   appId: "1:958094949964:web:a944742b6b6ab72822fee9",
+  //   measurementId: "G-VNNYHBJQJB"
+  // };
+
+  // // Initialize Firebase
+  // const app = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(app);
+
+  // console.log("app",app)
